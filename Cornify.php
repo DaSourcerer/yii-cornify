5b20f1931aa3f2082e8ba1019908672588b6df23
DaSourcerer <webmaster@dasourcerer.net>
2012-03-06 12:37:38 +0100

    relicensed under WTFPL

--- Cornify.php
+++ Cornify.php
@@ -1,25 +1,16 @@
 <?php
-/*   Copyright 2011 Da:Sourcerer
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */ 
 
 /**
  * This class simplifies the usage of the Cornify webservice within the Yii framework.
  * 
  * @author Da:Sourcerer
- * @version 1.0
- * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License 2
+ * @version 1.0.1
+ * @license http://sam.zoy.org/wtfpl/COPYING WTFPL 2.0
  */
 
 class Cornify extends CWidget {
